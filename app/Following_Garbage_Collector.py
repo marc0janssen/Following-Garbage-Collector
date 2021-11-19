7830e0e1215897255c535a703d65beabbb852511
Marco <5788988+marc0janssen@users.noreply.github.com>
2021-11-19 17:28:13 +0100

    logging

--- app/Following_Garbage_Collector.py
+++ app/Following_Garbage_Collector.py
@@ -137,7 +137,7 @@ class Follow_Garbage_Collector():
                             logging.info(
                                 f'Flushed @'
                                 f'{tweet["user"]["screen_name"]} '
-                                f'- {tweet["user"]["name"]}\n{tweetDate}\n'
+                                f'- {tweet["user"]["name"]} - {tweetDate} - '
                                 f'{tweet["text"]}'
                             )
 
